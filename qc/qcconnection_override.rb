f2628c390155dc1f789406fd52b081fb51ba8de6
Kristy Corkan <kcorkan@gmail.com>
2017-08-08 23:08:38 -0600

    added override for making crosslinkurl work

---
+++ qc/qcconnection_override.rb
@@ -0,0 +1,12 @@
+module RallyEIF
+  module WRK
+    class QCConnection
+
+    def get_object_link(artifact)
+            #s A link to a QC item looks like:
+            #    <a href="http://[qc_server_url]/??/??/ ...">bug_id</a>"
+            return nil
+          end
+    end
+  end
+end
